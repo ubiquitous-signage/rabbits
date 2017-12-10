d5e6759b76a9ca00d8032e0ab4e925ad4be88126
Jaga <salamander.g.1@gmail.com>
2017-12-10 20:29:57 +0900

    [update] contact with hamster

---
+++ card_reader/README.md
@@ -0,0 +1,11 @@
+python version 2.7
+
+dependent libraries
+
+```
+brew install usblib
+pip install nfcpy==0.12.0
+```
+
+## caution!!!!
+nfcpy correspond to python2.X and latest version is 0.13.4 but stabale version is 0.12.0
--- card_reader/card_reader.py
+++ card_reader/card_reader.py
@@ -1,42 +1,74 @@
 import binascii
 import nfc
-import pymongo
+import requests
+import json
+import signal
+import sys
+
+URL="http://localhost:9000/api/context"
 
 class MyCardReader(object):
+  map = {
+    '013200022d171900': 'ja',
+    '013200022d171a00': 'en',
+  }
 
-    def on_connect(self, tag):
-        print "touched"
-        self.idm = binascii.hexlify(tag.idm)
-        return True
+  def on_connect(self, tag):
+    print("touched")
+    self.idm = binascii.hexlify(tag.idm)
+    return True
 
-    def read_id(self):
-        clf = nfc.ContactlessFrontend('usb')
-        try:
-            clf.connect(rdwr={'on-connect': self.on_connect})
-        finally:
-            clf.close()
+  def read_id(self):
+    clf = nfc.ContactlessFrontend('usb')
+    try:
+      clf.connect(rdwr={'on-connect': self.on_connect})
+    finally:
+      clf.close()
 
-    def updateLang(self, cardId):
-        print cardId
-        client = pymongo.MongoClient('localhost', 27017)
-        db = client['ubiquitous-signage']
-        co = db.contexts
-        lang = 'en'
-        if cardId == '013200022d171900':
-            print 'ja!'
-            lang = 'ja'
-        elif cardId == '013200022d171a00':
-            print 'en!'
-            lang = 'en'
-        co.update({"id" : 0},{'$set': {'lang': lang}})
-        # co = db.posts.insert_one(post)
-        #co.insert_one(post)
+  def post(self, cardId):
+    lang = self.map[cardId]
+    print("detect: " + lang)
+    headers = {
+      'Content-Type': 'application/json',
+      'Accept':       'application/json',
+    }
+    payload = {
+      'Id'  : 0,
+      'Lang': lang,
+    }
+    try:
+      r = requests.post(URL, data=json.dumps(payload), headers=headers)
+      print('post status code: ' + str(r))
+    except requests.exceptions.ConnectionError:
+      print("please run hamster!")
+
+  def updateLang(self, cardId):
+    print(cardId)
+    client = pymongo.MongoClient('localhost', 27017)
+    db = client['ubiquitous-signage']
+    co = db.contexts
+    lang = 'en'
+    if cardId == '013200022d171900':
+      print('ja!')
+      lang = 'ja'
+    elif cardId == '013200022d171a00':
+      print('en!')
+      lang = 'en'
+    co.update({"id" : 0},{'$set': {'lang': lang}})
+    # co = db.posts.insert_one(post)
+    #co.insert_one(post)
+
+def handler(signal, frame):
+  print("exit")
+  sys.exit(0)
 
 if __name__ == '__main__':
-    cr = MyCardReader()
-    while True:
-        print "touch card:"
-        cr.read_id()
-        print "released"
-        cr.updateLang(cr.idm)
-        # print cr.idm
+  signal.signal(signal.SIGINT, handler)
+  cr = MyCardReader()
+  while True:
+    print("touch card:")
+    cr.read_id()
+    print("released")
+    cr.post(cr.idm)
+    # cr.updateLang(cr.idm)
+    # print cr.idm
